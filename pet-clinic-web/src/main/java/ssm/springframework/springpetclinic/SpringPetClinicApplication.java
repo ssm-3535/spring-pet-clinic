12afb8d3a2803f68310f713613f029c8f88283e5
sansanmoe <sansanmoe@192.168.11.6>
2020-09-23 00:05:37 +0900

    adding module

---
+++ pet-clinic-web/src/main/java/ssm/springframework/springpetclinic/SpringPetClinicApplication.java
@@ -0,0 +1,14 @@
+package ssm.springframework.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SpringPetClinicApplication.class, args);
+	}
+
+}

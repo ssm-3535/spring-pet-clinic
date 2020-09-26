1f5271b6fc97edf01ed49f450b7a1d71a6128e59
sansanmoe <sansanmoe@192.168.11.6>
2020-09-26 20:34:57 +0900

    Display Vets list #5

--- pet-clinic-web/src/main/java/ssm/springframework/springpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/ssm/springframework/springpetclinic/controller/VetController.java
@@ -1,13 +1,25 @@
 package ssm.springframework.springpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ssm.springframework.springpetclinic.services.VetService;
+
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 	
-	@RequestMapping({"/vets", "/vets/index","/vets/index.html"})
-	public String listVets() {
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({"", "/", "/index","/index.html"})
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }

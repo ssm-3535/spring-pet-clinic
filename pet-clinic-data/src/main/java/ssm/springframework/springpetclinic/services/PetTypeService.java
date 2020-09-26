c59bc715057f04901a1a45d194e49029907567e8
sansanmoe <sansanmoe@192.168.11.6>
2020-09-27 00:23:47 +0900

    Add PetType Service and PetType Service Map. closes #6

---
+++ pet-clinic-data/src/main/java/ssm/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package ssm.springframework.springpetclinic.services;
+
+import ssm.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+	
+}

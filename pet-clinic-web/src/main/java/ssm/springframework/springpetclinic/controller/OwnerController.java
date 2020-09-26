900e9ef57d249641de169ceaaa808c98a626a5c4
sansanmoe <sansanmoe@192.168.11.6>
2020-09-26 20:11:34 +0900

    Bind owner index and controller

--- pet-clinic-web/src/main/java/ssm/springframework/springpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/ssm/springframework/springpetclinic/controller/OwnerController.java
@@ -1,14 +1,25 @@
 package ssm.springframework.springpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ssm.springframework.springpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"", "/", "/index", "index.html"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
 	}
 }

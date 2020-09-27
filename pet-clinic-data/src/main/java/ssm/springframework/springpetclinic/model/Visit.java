46e95ec9c9b8cb913d5d34e4611df0f1107af108
sansanmoe <sansanmoe@192.168.11.6>
2020-09-27 17:59:55 +0900

    Add JPA Entities to Visits. Closes #14

--- pet-clinic-data/src/main/java/ssm/springframework/springpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/ssm/springframework/springpetclinic/model/Visit.java
@@ -2,9 +2,24 @@ package ssm.springframework.springpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
+	
+	@Column(name = "date")
 	private LocalDate date;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 	
 	public LocalDate getDate() {

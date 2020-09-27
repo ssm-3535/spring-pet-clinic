dbe0b875c507d72480f05c6558fa12c4514bda81
sansanmoe <sansanmoe@192.168.11.6>
2020-09-27 17:46:01 +0900

    Add Vats and Specialities to JPA Entities. closes #13

--- pet-clinic-data/src/main/java/ssm/springframework/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ssm/springframework/springpetclinic/model/Vet.java
@@ -3,8 +3,18 @@ package ssm.springframework.springpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 	
+	@ManyToMany
+	@JoinTable(name = "vets_specialities", joinColumns =  @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality> specialities = new HashSet<>();
 
 	public Set<Speciality> getSpecialities() {

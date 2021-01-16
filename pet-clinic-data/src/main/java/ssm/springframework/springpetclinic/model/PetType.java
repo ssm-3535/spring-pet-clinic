2d11e26d2d5c2e049bb7a331a7757d2cb43cc6cb
sansanmoe <sansanmoe3535@gmail.com>
2021-01-16 23:28:57 +0900

    Add toString() in PetType.java

--- pet-clinic-data/src/main/java/ssm/springframework/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ssm/springframework/springpetclinic/model/PetType.java
@@ -24,13 +24,9 @@ public class PetType extends BaseEntity{
 	@Column(name = "name")
 	private String name;
 
-	public String getName() {
+	@Override
+	public String toString() {
 		return name;
 	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
 	
 }
